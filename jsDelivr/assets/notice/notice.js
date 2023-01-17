7e20263079f56886602ce388ac2e5d278022686e
chettestwef <99162086+chettestwef@users.noreply.github.com>
2023-01-17 13:38:30 +0800

    Create notice.js

---
+++ jsDelivr/assets/notice/notice.js
@@ -0,0 +1,21 @@
+
+
+  //看啥看，不许看！
+
+
+
+
+swal({
+  title: "本站状态:出现故障",
+  text: "\n\n网站静态资源受到攻击，暂时无法访问，网站可能会出现显示错误等问题！",
+  icon: "warning",
+  
+  button: "好的",
+  dangerMode: true, //红色确认键
+});
+
+
+
+
+
+  // 提醒类型： success,error,warning.info
