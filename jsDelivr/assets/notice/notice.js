241b20fcbff5856f4a509f15e396da577f87b4f4
chettestwef <99162086+chettestwef@users.noreply.github.com>
2023-01-17 22:17:35 +0800

    Update notice.js

--- jsDelivr/assets/notice/notice.js
+++ jsDelivr/assets/notice/notice.js
@@ -1,21 +1,21 @@
-
-
   //看啥看，不许看！
 
-
-
-
-swal({
-  title: "本站状态:出现故障",
-  text: "2023/01/17 20:47\n\n网站静态资源服务器受到攻击，暂时无法访问，网站可能会出现显示错误等问题！",
-  icon: "warning",
+  Swal.fire({
+    icon: 'error',
+    confirmButtonText: '好的',
+    confirmButtonColor: '#FF6666',
+    title:'服务器公告',
+    html:
+    '<strong style="color:FF6666;font-size:25px">本站状态：一级故障</strong><br>' +
+    '<strong><font color="#00CCFF">日期：2023/01/17 21:39</strong>' +
+    '</br></br>因<b style="color:FF6666">网站静态资源服务器</b>受到攻击, ' +
+    '导致部分静态资源<b style="color:FF6666">无法加载</b>，<b style="color:FF6666">网站可能会</b>出现<b style="color:FF6666">显示错误</b>问题 ' +
+    '</br></br>您可以通过下方链接查看我们所有<b>服务</b>的<b>运行状态</b></font>',
+    footer: '<a style="color:#00CCFF" target="_blank" href="https://status.oneoc.xyz">服务状态检测系统</a>'
+  })
   
-  button: "好的",
-  dangerMode: true, //红色确认键
-});
-
-
-
-
-
-  // 提醒类型： success,error,warning.info
+  
+  
+  
+    // 提醒类型： success,error,warning.info,question
+  
